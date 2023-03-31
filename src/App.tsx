ab1f7cf480eb61d16e5c942be6d06655c065dea0
FailedSitcom <samuel@staffbase.com>
2023-03-31 15:12:18 +0200

    restructuring Shorter
    - moved all possible state inside Shorter component
    - updated Shorter to call onLinkChange function instead of hook methods directly
    - broke down handle-click into a few smaller helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
@@ -9,20 +9,16 @@ import Footer from "./components/Footer";
 function App() {
   const [originalLinks, setOriginalLinks] = useState<string[]>([]);
   const [links, setLinks] = useState<string[]>([]); // ??? Typescript Thingy
-  const [value, setValue] = useState("");
-  const [error, setError] = useState(false);
 
   return (
     <div>
       <NavBar />
       <Hero />
       <Shorter
-        value={value}
-        setValue={setValue}
-        setLinks={setLinks}
-        setOriginalLinks={setOriginalLinks}
-        error={error}
-        setError={setError}
+        onLinkChange={({originalLink, shortenedLink}) => {
+          setLinks([...links, shortenedLink]);
+          setOriginalLinks([...originalLinks, originalLink]);
+        }}
       />
       <Links links={links} originalLinks={originalLinks} />
       <Footer />
